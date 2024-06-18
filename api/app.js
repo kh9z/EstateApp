f66fd2d27ae9eba23fe17e8f25c7021c665ae321
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2024-06-18 17:46:11 +0300

    Create Routes

--- api/app.js
+++ api/app.js
@@ -1,10 +1,9 @@
 import express from "express";
+import postRoute from "./routes/post.route.js";
 
 const app = express();
 
-app.use("/api/test", (req,res)=>{
-    res.send("It works");
-})
+app.use("/api/posts", postRoute);
 
 app.listen(8800, ()=>{
     console.log("Server is running!");
