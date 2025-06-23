056b072c357e3d939bb1e5549c5670bad921d402
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2025-06-23 17:49:41 +0300

    main

---
+++ api/routes/post.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router()
+
+// router.get("/test", (req,res)=>{
+//     console.log("router work");
+// });
+// router.post("/test", (req,res)=>{
+//     console.log("router work");
+// });
+// router.put("/test", (req,res)=>{
+//     console.log("router work");
+// });
+// router.delete("/test", (req,res)=>{
+//     console.log("router work");
+// });
+
+export default router;
