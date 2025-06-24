89c168f672fd001d01bc2f2c469450a48b7405c7
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2025-06-24 20:53:46 +0300

    realise register & login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,28 +38,38 @@ export const login = async (req, res) => {
 
     //CHECK IF THE PASSWORD IS CORRECT
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid) return res.status(401).json({message:"Invalid Credentials!"})
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials!" });
 
     //GENERATE COOKIE TOKEN AND SEN TO THE USER
 
     // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
 
-    const age = 1000 * 60 * 60 * 24 * 7
+    const age = 1000 * 60 * 60 * 24 * 7;
 
-    const token = jwt.sign({
-      id:user.id
-    }, process.env.JWT_SECRET_KEY)
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
 
-    res.cookie("token", token, {
-      httpOnly:true,
-      // secure:true
-      maxAge:age,
-    }).status(200).json({message:"Login Successful"});
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to login!" });
   }
 };
 export const logout = (req, res) => {
-  res.clearCookie("token").status(200).json({message:"Logout Successful"})
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 };
