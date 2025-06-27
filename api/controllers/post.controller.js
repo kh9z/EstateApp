5f6bc750467eaa1a5b6f75f9e621dab164609635
Evgeny <vt211_fyeo@student.ztu.edu.ua>
2025-06-28 01:53:36 +0300

    Saved Post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,9 +1,24 @@
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = async (req, res) => {
+  const query = req.query;
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || 0,
+          lte: parseInt(query.maxPrice) || 100000000,
+        },
+      },
+    });
+    // setTimeout(() => {
     res.status(200).json(posts);
+    // }, 3000);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts" });
@@ -17,14 +32,31 @@ export const getPost = async (req, res) => {
       include: {
         postDetail: true,
         user: {
-            select:{
-                username:true,
-                avatar:true,
-            },
+          select: {
+            username: true,
+            avatar: true,
+          },
         },
       },
     });
-    res.status(200).json(post);
+    const token = req.cookies?.token;
+
+    if (token) {
+      return jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (!err) {
+          const saved = await prisma.savedPost.findUnique({
+            where: {
+              userId_postId: {
+                postId: id,
+                userId: payload.id,
+              },
+            },
+          });
+          return res.status(200).json({ ...post, isSaved: saved ? true : false });
+        }
+      });
+    }
+    return res.status(200).json({ ...post, isSaved: false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
